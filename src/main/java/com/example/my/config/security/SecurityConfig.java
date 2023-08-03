15b38731421660531e968c0f7836abb03e111a8f
DH <shim5013@naver.com>
2023-08-03 11:30:33 +0900

    csrf

---
+++ src/main/java/com/example/my/config/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.my.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 시큐리티를 사용하겠다
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // httpSecurity.csrf(
+        //         config -> config.disable());
+
+        return httpSecurity.getOrBuild();
+    }
+}
